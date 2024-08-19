5f23fc4cff37fca94d2f68e492ac9ac094266463
zhichenzzz <zczeng@uw.edu>
2024-08-19 05:49:07 +0000

    two kernels for predictor done

--- predictor_kernel/pooling_matmul.py
+++ predictor_kernel/pooling_matmul.py
@@ -29,7 +29,7 @@ def is_hip():
 configs = [
     triton.Config({'BLOCK_M': BM, 'BLOCK_N': BN}, num_stages=s, num_warps=w) \
     for BM in [64]\
-    for BN in [128]\
+    for BN in [64]\
     for s in ([1] if is_hip() else [3, 4, 7])\
     for w in [4, 8]\
 ]
@@ -52,7 +52,6 @@ class pooling_mm(torch.autograd.Function):
         K_HEADS = k.shape[1]
         HIDDEN_DIM = w.shape[-1]
         n_rep = N_HEADS // K_HEADS
-        # w n_head, hidden_dim, head_dim
         # shape constraints
         assert HEAD_DIM == w.shape[1]
         autotuned_config = max_pooling.configs[0]
@@ -60,7 +59,7 @@ class pooling_mm(torch.autograd.Function):
         q_down = torch.empty((BATCH, N_HEADS, N_CTX // BLOCK_M, HEAD_DIM), device=q.device, dtype=q.dtype)
         k_down = torch.empty((BATCH, K_HEADS, N_CTX // BLOCK_M, HIDDEN_DIM), device=q.device, dtype=q.dtype)
         n_d = triton.cdiv(q.shape[2], BLOCK_M)
-        o = torch.empty((BATCH, N_HEADS, n_d, w.shape[-1]), device=q.device, dtype=q.dtype)
+        o = torch.empty((BATCH, N_HEADS, n_d, n_d), device=q.device, dtype=q.dtype) - float("inf")
         extra_kern_args = {}
         # Tuning for AMD target
         if is_hip():
@@ -77,15 +76,171 @@ class pooling_mm(torch.autograd.Function):
             N_HEADS, N_CTX, HEAD_DIM, HIDDEN_DIM,
             **extra_kern_args,)
         
-        p = torch.matmul(q_down, w)
-        k = k_down[:, :, None, :, :].expand(BATCH, K_HEADS, N_HEADS // K_HEADS, N_CTX // BLOCK_M, HIDDEN_DIM).reshape(BATCH, N_HEADS, N_CTX // BLOCK_M, HIDDEN_DIM)
-        o = torch.matmul(p, k.transpose(-1, -2))
-        M = torch.tril(torch.ones((n_d, n_d), device=q.device))
-        o[:, :, M == 0] = float("-inf")
+        grid = lambda args: (triton.cdiv(n_d, args["BLOCK_M"]), BATCH * N_HEADS, 1)
+        
+        # qw = torch.matmul(q_down, w)
+        # matmul_kernel_causal[grid](
+        #     qw, k_down, o,
+        #     qw.stride(0), qw.stride(1), qw.stride(2), qw.stride(3),
+        #     k_down.stride(0), k_down.stride(1), k_down.stride(2), k_down.stride(3),
+        #     o.stride(0), o.stride(1), o.stride(2), o.stride(3),
+        #     N_HEADS, n_rep, n_d, HIDDEN_DIM,
+        #     **extra_kern_args,)
+        
+        fused_matmul[grid](
+            q_down, k_down, w, o, n_rep,
+            q_down.stride(0), q_down.stride(1), q_down.stride(2), q_down.stride(3),
+            k_down.stride(0), k_down.stride(1), k_down.stride(2), k_down.stride(3),
+            w.stride(0), w.stride(1), w.stride(2),
+            o.stride(0), o.stride(1), o.stride(2), o.stride(3),
+            N_HEADS, n_d, HEAD_DIM, HIDDEN_DIM, 
+            **extra_kern_args,)
+        
         nnz_id = torch.topk(o, 5, dim=-1).indices
 
         return nnz_id
-   
+
+@triton.autotune(list(filter(keep, configs)), key=["n_d", "HIDDEN_DIM"])
+@triton.jit
+def matmul_kernel_causal(
+        a_ptr, b_ptr, c_ptr,
+        stride_az, stride_ah, stride_am, stride_ak,  #
+        stride_bz, stride_bh, stride_bk, stride_bn,  #
+        stride_cz, stride_ch, stride_cm, stride_cn,  #
+        H, n_rep,
+        n_d: tl.constexpr,
+        HIDDEN_DIM: tl.constexpr,
+        BLOCK_M: tl.constexpr, 
+        BLOCK_N: tl.constexpr, 
+):
+    start_m = tl.program_id(axis=0)
+    off_hz = tl.program_id(axis=1)
+    off_z = off_hz // H
+    off_h = off_hz % H
+    off_bh = off_h // n_rep
+    a_offset = off_z.to(tl.int64) * stride_az + off_h.to(tl.int64) * stride_ah
+    b_offset = off_z.to(tl.int64) * stride_bz + off_bh.to(tl.int64) * stride_bh
+    c_offset = off_z.to(tl.int64) * stride_cz + off_h.to(tl.int64) * stride_ch
+
+    a_block_ptr = tl.make_block_ptr(
+        base = a_ptr + a_offset,
+        shape=(BLOCK_M, HIDDEN_DIM),
+        strides=(stride_am, stride_ak),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, HIDDEN_DIM),
+        order=(0, 1)
+    )
+    b_block_ptr = tl.make_block_ptr(
+        base = b_ptr + b_offset,
+        shape=(HIDDEN_DIM, n_d),
+        strides=(stride_bn, stride_bk),
+        offsets=(0, 0),
+        block_shape=(HIDDEN_DIM, BLOCK_N),
+        order=(0, 1)
+    )
+    c_block_ptr = tl.make_block_ptr(
+        base = c_ptr + c_offset,
+        shape=(BLOCK_M, n_d),
+        strides=(stride_cm, stride_cn),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, BLOCK_N),
+        order=(0, 1)
+    )
+
+    offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_n = tl.arange(0, BLOCK_N)
+    lo, hi = 0, (start_m + 1) * BLOCK_M
+
+    for start_n in range(lo, hi, BLOCK_N):
+        q = tl.load(a_block_ptr)
+        k = tl.load(b_block_ptr)
+        qk = tl.dot(q, k)
+        mask = offs_m[:, None] >= (start_n + offs_n[None, :])
+        qk = qk + tl.where(mask, 0, -1.0e6)
+        qk = qk.to(tl.float16)
+        tl.store(c_block_ptr, qk)
+        b_block_ptr = tl.advance(b_block_ptr, (0, BLOCK_N))
+        c_block_ptr = tl.advance(c_block_ptr, (0, BLOCK_N))  
+
+
+@triton.autotune(list(filter(keep, configs)), key=["n_d", "HEAD_DIM"])
+@triton.jit
+def fused_matmul(Q, K, W, O, n_rep,  #
+                stride_qz, stride_qh, stride_qm, stride_qk,  #
+                stride_kz, stride_kh, stride_km, stride_kn,  #
+                stride_wh, stride_wk, stride_wn,  #
+                stride_oz, stride_oh, stride_om, stride_on,  #
+                H, 
+                n_d: tl.constexpr,  #
+                HEAD_DIM: tl.constexpr,  #
+                HIDDEN_DIM: tl.constexpr,  #
+                BLOCK_M: tl.constexpr,  #
+                BLOCK_N: tl.constexpr,  #
+                ):
+    start_m = tl.program_id(0)
+    off_hz = tl.program_id(1)
+    off_z = off_hz // H
+    off_h = off_hz % H
+    off_kh = off_h // n_rep
+    q_offset = off_z.to(tl.int64) * stride_qz + off_h.to(tl.int64) * stride_qh
+    k_offset = off_z.to(tl.int64) * stride_kz + off_kh.to(tl.int64) * stride_kh
+    w_offset = off_h.to(tl.int64) * stride_wh
+    o_offset = off_z.to(tl.int64) * stride_oz + off_h.to(tl.int64) * stride_oh
+    # block pointers
+    Q_block_ptr = tl.make_block_ptr(
+        base=Q + q_offset,
+        shape=(n_d, HEAD_DIM),
+        strides=(stride_qm, stride_qk),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, HEAD_DIM),
+        order=(0, 1),
+    )
+    W_block_ptr = tl.make_block_ptr(
+        base=W + w_offset,
+        shape=(HEAD_DIM, HIDDEN_DIM),
+        strides=(stride_wk, stride_wn),
+        offsets=(0, 0),
+        block_shape=(HEAD_DIM, BLOCK_N),
+        order=(0, 1),
+    )
+    K_block_ptr = tl.make_block_ptr(
+        base=K + k_offset,
+        shape=(HIDDEN_DIM, n_d),
+        strides=(stride_kn, stride_km),
+        offsets=(0, 0),
+        block_shape=(BLOCK_N, BLOCK_M),
+        order=(1, 0),
+    )
+    O_block_ptr = tl.make_block_ptr(
+        base=O + o_offset,
+        shape=(BLOCK_M, n_d),
+        strides=(stride_om, stride_on),
+        offsets=(start_m * BLOCK_M, 0),
+        block_shape=(BLOCK_M, BLOCK_M),
+        order=(0, 1),
+    )
+    q = tl.load(Q_block_ptr)
+    lo, hi = 0, (start_m + 1) * BLOCK_M
+    for start_k in range(lo, hi, BLOCK_M):
+        acc = tl.zeros((BLOCK_M, BLOCK_M), dtype=tl.float16)
+        for start_n in range(0, HIDDEN_DIM, BLOCK_N):
+            
+            w = tl.load(tl.advance(W_block_ptr, (0, start_n)))
+            qw = tl.dot(q, w).to(tl.float16)
+            k = tl.load(tl.advance(K_block_ptr, (start_n, 0)))
+            o = tl.dot(qw, k).to(tl.float16)
+            acc = acc + o
+        offs_m = start_m * BLOCK_M + tl.arange(0, BLOCK_M)
+        offs_n = start_k + tl.arange(0, BLOCK_M)
+        mask = offs_m[:, None] >= offs_n[None, :]
+        acc = acc + tl.where(mask, 0, -1.0e6)
+        acc = acc.to(tl.float16)
+        # print(acc.shape)
+        tl.store(O_block_ptr, acc)
+        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_M))
+        O_block_ptr = tl.advance(O_block_ptr, (0, BLOCK_M))
+
+
 @triton.autotune(list(filter(keep, configs)), key=["N_CTX", "HEAD_DIM"])
 @triton.jit
 def max_pooling(Q, K, Q_down, K_down, n_rep,  #
@@ -143,7 +298,7 @@ def max_pooling(Q, K, Q_down, K_down, n_rep,  #
         order=(0, 1),
     )
     q = tl.load(Q_block_ptr)
-    q = tl.sum(q, 0).to(tl.float16)[None, :] / BLOCK_M
+    q = tl.sum(q, 0)[None, :] / BLOCK_M
     tl.store(Q_down_block_ptr, q.to(Q_down.type.element_ty))
 
     # K Dowmsample
